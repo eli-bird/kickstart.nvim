8f3fedaa7f643a4d3b32048594528abae1b1e409
Eli Bird <nerdkatt77@gmail.com>
2024-08-03 01:24:43 -0400

    merge

--- init.lua
+++ init.lua
@@ -91,7 +91,6 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
@@ -103,7 +102,6 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
 vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
